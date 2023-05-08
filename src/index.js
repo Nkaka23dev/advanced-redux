1eaf157d394707fe73337def195ecfea2fd136c7
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-08 15:44:15 +0300

    feat: started firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello There")
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSZB-CmomA5mdjICWXAc9YQBH_OlSqQpc",
+    authDomain: "initialproject-de653.firebaseapp.com",
+    projectId: "initialproject-de653",
+    storageBucket: "initialproject-de653.appspot.com",
+    messagingSenderId: "608495829670",
+    appId: "1:608495829670:web:2e8762606639f20f526a67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const databaseRef = collection(db, 'firabase-dev');
+
+// console.log(databaseRef)
+
+const querySnapshot = await getDocs(databaseRef);
+
+// querySnapshot.forEach((doc) => {
+//     console.log(doc.data());
+// });
+
+console.log(querySnapshot.doc)
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  experiments: {
+    topLevelAwait: true,
+  },
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  watch: true, // Enable watch mode
-  watchOptions: {
-    ignored: /node_modules/, // Ignore changes to node_modules directory
-  },
-  mode: "development"
+  watch: true
 };
